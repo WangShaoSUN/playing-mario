1df02e381f3da63b997098b4171241fe1767f393
samwang <samwang@mail.ustc.edu.cn>
2021-05-06 15:14:08 +0800

    Update env_wrapper.py

--- env_wrapper.py
+++ env_wrapper.py
@@ -6,6 +6,7 @@ from collections import  deque
 from multiprocessing import  Process,Pipe
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
+
 from abc import ABC,abstractmethod
 print("gym version:",gym.__version__)
 import matplotlib.pyplot as plt
@@ -67,38 +68,143 @@ class ClipReward(gym.RewardWrapper):
          return np.sign(reward)
 
 class LazyFrames(object):
-    def __init__(self,frames):
-        self._frame=frames
+    r"""Ensures common frames are only stored once to optimize memory use.
+
+    To further reduce the memory use, it is optionally to turn on lz4 to
+    compress the observations.
+
+    .. note::
+
+        This object should only be converted to numpy array just before forward pass.
+
+    Args:
+        lz4_compress (bool): use lz4 to compress the frames internally
+
+    """
+    __slots__ = ('frame_shape', 'dtype', 'shape', 'lz4_compress', '_frames')
+
+    def __init__(self, frames, lz4_compress=False):
+        self.frame_shape = tuple(frames[0].shape)
+        self.shape = (len(frames),) + self.frame_shape
+        self.dtype = frames[0].dtype
+        if lz4_compress:
+            from lz4.block import compress
+            frames = [compress(frame) for frame in frames]
+        self._frames = frames
+        self.lz4_compress = lz4_compress
 
     def __array__(self, dtype=None):
-        out = np.concatenate(self._frame, axis=0)
+        arr = self[:]
         if dtype is not None:
-            out = out.astype(dtype)
-        return out
+            return arr.astype(dtype)
+        return arr
+
+    def __len__(self):
+        return self.shape[0]
+
+    def __getitem__(self, int_or_slice):
+        if isinstance(int_or_slice, int):
+            return self._check_decompress(self._frames[int_or_slice])  # single frame
+        return np.stack([self._check_decompress(f) for f in self._frames[int_or_slice]], axis=0)
+
+    def __eq__(self, other):
+        return self.__array__() == other
+
+    def _check_decompress(self, frame):
+        if self.lz4_compress:
+            from lz4.block import decompress
+            return np.frombuffer(decompress(frame), dtype=self.dtype).reshape(self.frame_shape)
+        return frame
+
 
 class FrameStack(gym.Wrapper):
-    def __init__(self,env,k):
-        " stack the last k frames"
-        # gym.wrappers.__init__(self,env)
-        super(FrameStack,self).__init__(env)
-        self.k=k
-        self.frames=deque([],maxlen=k)
-        shape=env.observation_space.shape
-        self.observation_space = spaces.Box(low=0, high=255, shape=(shape[0] * k, shape[1], shape[2]))
+    r"""Observation wrapper that stacks the observations in a rolling manner.
 
-    def reset(self):
-        obs=self.env.reset()
-        for _ in range(self.k):
-            self.frames.append(obs)
-        return self._get_obs()
+    For example, if the number of stacks is 4, then the returned observation contains
+    the most recent 4 observations. For environment 'Pendulum-v0', the original observation
+    is an array with shape [3], so if we stack 4 observations, the processed observation
+    has shape [4, 3].
+
+    .. note::
+
+        To be memory efficient, the stacked observations are wrapped by :class:`LazyFrame`.
+
+    .. note::
+
+        The observation space must be `Box` type. If one uses `Dict`
+        as observation space, it should apply `FlattenDictWrapper` at first.
+
+    Example::
+
+        >>> import gym
+        >>> env = gym.make('PongNoFrameskip-v0')
+        >>> env = FrameStack(env, 4)
+        >>> env.observation_space
+        Box(4, 210, 160, 3)
+
+    Args:
+        env (Env): environment object
+        num_stack (int): number of stacks
+        lz4_compress (bool): use lz4 to compress the frames internally
+
+    """
+    def __init__(self, env, num_stack, lz4_compress=False):
+        super(FrameStack, self).__init__(env)
+        self.num_stack = num_stack
+        self.lz4_compress = lz4_compress
+
+        self.frames = deque(maxlen=num_stack)
+
+        low = np.repeat(self.observation_space.low[np.newaxis, ...], num_stack, axis=0)
+        high = np.repeat(self.observation_space.high[np.newaxis, ...], num_stack, axis=0)
+        self.observation_space = gym.spaces.Box(low=low, high=high, dtype=self.observation_space.dtype)
+
+    def _get_observation(self):
+        assert len(self.frames) == self.num_stack, (len(self.frames), self.num_stack)
+        return LazyFrames(list(self.frames), self.lz4_compress)
 
     def step(self, action):
-        obs, reward, done, info = self.env.step(action)
-        self.frames.append(obs)
-        return self._get_obs(), reward, done, info
-    def _get_obs(self):
-        assert len(self.frames) == self.k
-        return LazyFrames(list(self.frames)).__array__(np.float)
+        observation, reward, done, info = self.env.step(action)
+        self.frames.append(observation)
+        return self._get_observation(), reward, done, info
+
+    def reset(self, **kwargs):
+        observation = self.env.reset(**kwargs)
+        [self.frames.append(observation) for _ in range(self.num_stack)]
+        return self._get_observation()
+# class LazyFrames(object):
+#     def __init__(self,frames):
+#         self._frame=frames
+#
+#     def __array__(self, dtype=None):
+#         out = np.concatenate(self._frame, axis=0)
+#         if dtype is not None:
+#             out = out.astype(dtype)
+#         return out
+#
+# class FrameStack(gym.Wrapper):
+#     def __init__(self,env,k):
+#         " stack the last k frames"
+#         # gym.wrappers.__init__(self,env)
+#         super(FrameStack,self).__init__(env)
+#         self.k=k
+#         self.frames=deque([],maxlen=k)
+#         shape=env.observation_space.shape
+#         self.observation_space = spaces.Box(low=0, high=255, shape=(shape[0] * k, shape[1], shape[2]))
+#
+#     def reset(self):
+#         obs=self.env.reset()
+#         for _ in range(self.k):
+#             self.frames.append(obs)
+#         return self._get_obs()
+#
+#     def step(self, action):
+#         obs, reward, done, info = self.env.step(action)
+#         self.frames.append(obs)
+#         return self._get_obs(), reward, done, info
+#     def _get_obs(self):
+#         assert len(self.frames) == self.k
+#         return LazyFrames(list(self.frames)).__array__(np.float)
 
 class  VecEnv(ABC):
     def __init__(self,num_envs,observation_space,action_space):
@@ -147,9 +253,7 @@ def worker(remote,paraent_remote,env):
             # env.render()
             remote.send((ob, reward, done, info))
         elif cmd  == 'reset':
-
             ob = env.reset()
-
             remote.send(ob)
         elif cmd == 'reset_task':
             ob = env.reset_task()
@@ -244,8 +348,11 @@ if __name__ == '__main__':
 
 
 
-    env = SubprocVecEnv([wrap_cover("d", 3) for i in range(5)])
-    print(env.action_space.n)
+    env = SubprocVecEnv([wrap_cover("d", 3) for i in range(6)])
+    # print(env.action_space.n)
     print(env.observation_space.shape)
-    s=np.array(env.reset())
+    s=(env.reset())
+    print(type(s))
+    print(s.shape)
+    s=np.squeeze(s,2)
     print(s.shape)
